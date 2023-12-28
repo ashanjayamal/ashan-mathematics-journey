fd002ee8fb09b30079794e3f2db55744cf40992c
ashanjayamal <115121151+ashanjayamal@users.noreply.github.com>
2023-12-28 06:21:25 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
 window.onload = function() {
-    var definitions = document.getElementsByClassName('definition').length;
-    var theorems = document.getElementsByClassName('theorem').length;
-    alert('There are ' + definitions + ' definitions and ' + theorems + ' theorems on this page.');
+    var definitions = document.getElementsByClassName('definition');
+    for (var i = 0; i < definitions.length; i++) {
+        definitions[i].getElementsByClassName('def-number')[0].innerText = i + 1;
+    }
+    var theorems = document.getElementsByClassName('theorem');
+    for (var i = 0; i < theorems.length; i++) {
+        theorems[i].getElementsByClassName('thm-number')[0].innerText = definitions.length + i + 1;
+    }
 }
