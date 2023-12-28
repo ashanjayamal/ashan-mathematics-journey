d7c1e91e99d24b0fd41e8bb344a63134df313911
ashanjayamal <115121151+ashanjayamal@users.noreply.github.com>
2023-12-28 06:15:35 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    var definitions = document.getElementsByClassName('definition').length;
+    var theorems = document.getElementsByClassName('theorem').length;
+    alert('There are ' + definitions + ' definitions and ' + theorems + ' theorems on this page.');
+}
